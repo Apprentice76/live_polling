ca8b6631ebd0d05f0a926258c5650ffeba54ea82
Apprentice76 <hrk.som.01@gmail.com>
2022-10-08 17:54:43 +0530

    progress 08.10.22 17.44

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,5 +1,15 @@
+import Card from '../Card/Card'
+import bg from '../../assets/img/background.jpg'
+import './App.css'
+
 const App = () => {
-	return <div className='App'></div>
+	return (
+		<div className='app' style={{ background: `url(${bg}) no-repeat center` }}>
+			<div className='container'>
+				<Card />
+			</div>
+		</div>
+	)
 }
 
 export default App
